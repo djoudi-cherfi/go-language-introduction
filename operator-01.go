5cd34d11a088f99e868d938b0dc7e2fb36fe7ade
djoudi-cherfi <djoudi.cherfi@gmail.com>
2022-09-27 20:38:08 +0200

    Introduction aux opérateurs relationnels et logiques

--- operator-01.go
+++ operator-01.go
@@ -3,20 +3,38 @@ package main
 import "fmt"
 
 func operatorOne() {
-	// Relational == != < <= > >=
-
 	var (
 		x int
 		y int
 	)
 
-	x = 7
-	y = 3
+	// x = 7
+	// y = 3
+
+	x = 15
+	y = 15
 
 	// Arithmetic + - / * %
+	fmt.Println("----------- Arithmetic")
 	fmt.Println(x + y)
 	fmt.Println(x - y)
 	fmt.Println(x / y)
 	fmt.Println(x * y)
 	fmt.Println(x % y) // % the remainder of the division
+
+	// Relational == != < > <= >=
+	fmt.Println("----------- Relational")
+	fmt.Println(x == y)
+	fmt.Println(x != y)
+	fmt.Println(x < y)
+	fmt.Println(x > y)
+	fmt.Println(x <= y)
+	fmt.Println(x >= y)
+
+	// Logical && ||
+	fmt.Println("----------- Logical")
+	fmt.Println(x == y && x != y)
+	fmt.Println(x > y && x != y)
+	fmt.Println(x == y || x != y)
+	fmt.Println(x > y || x != y)
 }
